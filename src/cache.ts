6820a74ce396abef7c0886a5a8c86c0ad57e056b
Burak Tasci <burak.tasci@cozumtech.net>
2017-04-28 16:48:47 +0300

    style: lint

--- src/cache.ts
+++ src/cache.ts
@@ -2,7 +2,7 @@
 import { CacheValue } from './models/cache-value';
 
 export abstract class Cache {
-  abstract get keys(): Array<string> ;
+  abstract get keys(): Array<string>;
   abstract getItem(key: string): CacheValue;
   abstract setItem(key: string, value: CacheValue): boolean;
   abstract removeItem(key: string): void;
